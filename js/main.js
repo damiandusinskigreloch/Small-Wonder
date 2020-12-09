8755cf865216c8f90ee894f91bc4bb170fa85813
damiandusinskigreloch <damiandusinskiggreloch@gmail.com>
2020-12-09 15:37:27 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+  document.querySelector('.menu-container').classList.toggle('show');
+  document.querySelector('.menu-btn').classList.toggle('show');
+});
+
