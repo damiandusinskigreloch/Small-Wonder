9f13a7c34ae7fea73304d6820f0ca5293ed4656d
damiandusinskigreloch <damiandusinskiggreloch@gmail.com>
2020-12-10 17:50:20 +0100

    create scss file structure

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menuContainer = document.querySelector('.menu-container');
-
 menuBtn.addEventListener('click', () => {
   menuContainer.classList.toggle('show');
   menuBtn.classList.toggle('show');
